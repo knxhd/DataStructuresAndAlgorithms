ecfd652fe133bf2cd7b15044ced00334673424a8
mikasa007 <17863109506@163.com>
2019-10-25 15:28:26 +0800

    测试一下冲突的解决方法

--- src/main/java/com/knxhd/sort/Example.java
+++ src/main/java/com/knxhd/sort/Example.java
@@ -7,27 +7,33 @@ package com.knxhd.sort;
  * @date 2019/10/25 12:21
  */
 public class Example {
-    public static void sort(Comparable[] a) {
 
+
+    public static void sort(Comparable[] a) {
+        System.out.println("排序一下");
     }
 
     public static boolean less(Comparable<Object> o1, Comparable<Object> o2) {
+        System.out.println("比较两个值的大小");
         return o1.compareTo(o2) < 0;
     }
 
     public static void exch(Comparable[] o, int i, int j) {
+        System.out.println("交换两个值");
         Comparable temp = o[i];
         o[i] = o[j];
         o[j] = temp;
     }
 
     public static void show(Comparable[] o) {
+        System.out.println("展示排序后的数据");
         for (Comparable comparable : o) {
             System.out.println(comparable + "");
         }
     }
 
     public static boolean isSortd(Comparable<Object>[] o) {
+        System.out.println("判断是否是有顺序的");
         for (int i = 1; i < o.length; i++) return !less(o[i], o[i - 1]);
         return true;
     }
